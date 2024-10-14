318f2c1b06e26efd7a4fcd3384dbdc2f14bb3595
Ojas Dhotre <ojasdhotre9@gmail.com>
2024-10-14 17:07:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Heloo all</h1>
+      <h3>I am Ojas 🥽</h3>
+    </>
+  );
+}
+
+export default App;
